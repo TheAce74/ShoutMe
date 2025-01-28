c7aaa2f534a49bb728c1c91a6008d357d23e5a9a
Chisom Udonsi <udonsichisom02@gmail.com>
2025-01-28 16:33:12 -0500

    fix: minor deployment fix

--- src/app/(landing)/auth/login/page.tsx
+++ src/app/(landing)/auth/login/page.tsx
@@ -1,10 +1,16 @@
 import Login from "@/components/ui/auth/Login";
+import Loader from "@/components/ui/loader";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Login",
 };
 
 export default function page() {
-  return <Login />;
+  return (
+    <Suspense fallback={<Loader />}>
+      <Login />
+    </Suspense>
+  );
 }
