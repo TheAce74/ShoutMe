d2b958d0aa1e5bf02a0fb91dbd8f427343622b35
Chisom Udonsi <udonsichisom02@gmail.com>
2025-01-28 21:25:21 -0500

    feat: made app a pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { cn } from "@/lib/utils";
@@ -10,13 +10,50 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
+const APP_NAME = "ShoutMe";
+const APP_DEFAULT_TITLE = "ShoutMe";
+const APP_TITLE_TEMPLATE = "%s | ShoutMe";
+const APP_DESCRIPTION =
+  "ShoutMe is a real-time emergency alert system designed for tertiary institutions, enabling rapid alert dissemination, prompt crisis response, and an intuitive platform for reporting and monitoring emergencies to enhance safety and security.";
+
 export const metadata: Metadata = {
+  applicationName: APP_NAME,
   title: {
-    template: "%s | ShoutMe",
-    default: "ShoutMe",
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
   },
-  description:
-    "ShoutMe is a real-time emergency alert system designed for tertiary institutions, enabling rapid alert dissemination, prompt crisis response, and an intuitive platform for reporting and monitoring emergencies to enhance safety and security.",
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
